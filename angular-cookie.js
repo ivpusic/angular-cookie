0a587c0766c687beee072f8529070356dbba9b4f
Matjaz Lipus <matjazl@gmail.com>
2013-10-03 09:52:54 +0200

    bug fixes & renamed module

--- angular-cookie.js
+++ angular-cookie.js
@@ -1,106 +1,83 @@
-/*
+/**
 * Copyright 2013 Ivan Pusic
+* Contributors:
+*   Matjaz Lipus
 */
-(function(angular, undefined) {
+angular.module('myCookie', []).factory('Cookie', ['$document', function ($document) {
     'use strict';
 
-    function extend(a, b){
-        var key;
-        for(key in b) {
-            if(b.hasOwnProperty(key)) {
-                a[key] = b[key];
+    function cookieFun(key, value, options) {
+
+        var i,
+            len,
+            pos,
+            list,
+            name,
+            cookie,
+            cookies,
+            expires,
+            hasCookies;
+
+        // we are getting value
+        if (value !== undefined) {
+            // we are setting value
+            value = typeof value === 'object' ? JSON.stringify(value) : String(value);
+
+            if (options && typeof options.expires === 'number') {
+                expires = new Date();
+                expires.setDate(expires.getDate() + options.expires);
+                options.expires = expires;
             }
+
+            $document.context.cookie =
+                encodeURIComponent(key) + '=' + encodeURIComponent(value) +
+                (options && options.expires ? '; expires=' + options.expires.toUTCString() : '') +
+                (options && options.path    ? '; path='    + options.path                  : '') +
+                (options && options.domain  ? '; domain='  + options.domain                : '') +
+                (options && options.secure  ? '; secure'                                   : '');
+            return;
         }
-        return a;
-    }
 
-    function isEmpty(obj) {
+        // we are getting value
+        hasCookies = false;
+        cookies    = {};
+        list       = $document.context.cookie.split('; ');
+        for (i = 0, len = list.length; i < len; i += 1) {
+            if (list[i]) {
+                cookie = list[i];
+                pos = cookie.indexOf('=');
+                name = cookie.substring(0, pos);
+                value = decodeURIComponent(cookie.substring(pos + 1));
 
-        if (obj === null || obj === undefined) {
-            return false;
-        }
-
-        var key;
-        for(key in obj) {
-            if(obj.hasOwnProperty(key)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    angular.module('ngCookie', ['ng']).
-    factory('$cookie', ['$document', function ($document) {
-        return (function() {
-            function cookieFun(key, value, options) {
-
-                options = options || {};
-
-                if (value !== undefined) {
-                    // we are setting value
-                    value = typeof value === 'object' ? JSON.stringify(value) : String(value);
-
-                    if (typeof options.expires === 'number') {
-                        var expiresFor = options.expires;
-                        options.expires = new Date();
-                        // Trying to delete a cookie; set a date far in the past
-                        if(expiresFor === -1) {
-                            options.expires = new Date('Thu, 01 Jan 1970 00:00:00 GMT');
-                            // A new 
-                        } else {
-                            options.expires.setDate(options.expires.getDate() + expiresFor);    
-                        }
-                    }   
-                    return ($document[0].cookie = [
-                        encodeURIComponent(key),
-                        '=',
-                        encodeURIComponent(value),
-                        options.expires ? '; expires=' + options.expires.toUTCString() : '',
-                        options.path    ? '; path=' + options.path : '',
-                        options.domain  ? '; domain=' + options.domain : '',
-                        options.secure  ? '; secure' : ''
-                        ].join('')); 
-                }
-
-                var cookies = {}, list = [], i, cookie, pos, name;
-
-                var all = $document[0].cookie;
-                if (all) {
-                    list = all.split("; ");
-                }
-
-                for(i = 0; i < list.length; ++i) {  
-                    if (list[i]) {
-                        cookie = list[i];
-                        pos = cookie.indexOf("=");        
-                        name = cookie.substring(0, pos);
-                        value = decodeURIComponent(cookie.substring(pos + 1));
-
-                        if (key === undefined || key === name) {
-                            try {
-                                cookies[name] = JSON.parse(value);
-                            } catch (e) {
-                                cookies[name] = value;
-                            }
-                            if (key === name) {
-                                return cookies[name];
-                            }
-                        }
+                if (key === undefined || key === name) {
+                    try {
+                        cookies[name] = JSON.parse(value);
+                    } catch (e) {
+                        cookies[name] = value;
                     }
+                    if (key === name) {
+                        return cookies[name];
+                    }
+                    hasCookies = true;
                 }
-                return isEmpty(cookies) ? false : cookies;
             }
-            cookieFun.remove = function (key, options) {
+        }
+        if (hasCookies && key === undefined) {
+            return cookies;
+        }
+    }
 
-                options = options || {};
+    cookieFun.remove = function (key, options) {
 
-                if (cookieFun(key) !== undefined) {
-                    cookieFun(key, '', extend(options, { expires: -1 }));
-                    return true;
-                }
-                return false;
-            };
-            return cookieFun;
-        }());
-    }]);
-}(window.angular));
+        var hasCookie = cookieFun(key) !== undefined;
+        if (hasCookie) {
+            if (!options) {
+                options = {};
+            }
+            options.expires = -1;
+            cookieFun(key, '', options);
+        }
+        return hasCookie;
+    };
+    return cookieFun;
+}]);
